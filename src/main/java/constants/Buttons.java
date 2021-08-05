ed3dc4fa54c9a6396a884d98abb963a54bf1a047
rokaskvasas <rokas.kvasas@gmail.com>
2021-08-05 14:45:45 +0300

    Added colors to buttons

---
+++ src/main/java/constants/Buttons.java
@@ -0,0 +1,9 @@
+package constants;
+
+public class Buttons {
+    public static final String oneH = "in 1 Hour";
+    public static final String twoH = "in 2 Hour";
+    public static final String abort ="Abort";
+    public static final String seconds = "Enter seconds:";
+    public static final String entText = "till shutdown";
+}
--- src/main/java/gui/MainFrame.java
+++ src/main/java/gui/MainFrame.java
@@ -1,9 +1,13 @@
 package gui;
 
+import constants.Buttons;
 import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
 @SuppressWarnings("FieldCanBeLocal")
 
 public class MainFrame extends JFrame {
@@ -24,11 +28,16 @@ public class MainFrame extends JFrame {
                 "40[]40[]40[]",
                 "80[]20[]"
         ));
-        b1shut = new JButton("in 1 hour");
-        b2shut = new JButton("in 2 hour");
-        b3abort = new JButton("ABORT!!");
-        timeLabel = new JLabel("Enter seconds till shutoff");
+        b1shut = new JButton(Buttons.oneH);
+        b2shut = new JButton(Buttons.twoH);
+        b3abort = new JButton(Buttons.abort);
+        timeLabel = new JLabel(Buttons.seconds);
         seconds = new JTextField(10);
+        seconds.setText(Buttons.entText);
+        b1shut.setBackground(Color.orange);
+        b2shut.setBackground(new Color(0,204,0));
+        b3abort.setBackground(new Color(255,80,0));
+
 
         //region add to frame
         this.add(background);
@@ -37,6 +46,26 @@ public class MainFrame extends JFrame {
         background.add(b3abort,"wrap");
         background.add(timeLabel,"split 2, span 3");
         background.add(seconds);
+        //endregion
+        //region Listener
+        seconds.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if(seconds.getText().equals(Buttons.entText)){
+                    seconds.setText("");
+                    seconds.setForeground(Color.BLACK);
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if(seconds.getText().isEmpty()){
+                    seconds.setText(Buttons.entText);
+                    seconds.setForeground(Color.GRAY);
+                }
+
+            }
+        });
         //endregion
 
 
